c766266b06ccf74791f29ce4fdbbcb12cb63e70b
Nelson Fonkoua <tambue@hotmail.fr>
2023-01-12 07:57:37 +0100

    added MarcelLexer

--- marcel-lexer/src/main/java/com/tambapps/marcel/lexer/MarcelLexer.java
+++ marcel-lexer/src/main/java/com/tambapps/marcel/lexer/MarcelLexer.java
@@ -1,14 +1,29 @@
 package com.tambapps.marcel.lexer;
 
-import java.util.Collections;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.List;
 
-// TODO use flex
-// https://jflex.de/manual.html#Example
 public class MarcelLexer {
 
-
-  List<LexToken> lex(String content) {
-    return Collections.emptyList();
+  public List<LexToken> lex(String content) throws MarcelLexerException {
+    try (Reader reader = new StringReader(content)) {
+      return lex(reader);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
+
+  public List<LexToken> lex(Reader content) throws IOException, MarcelLexerException {
+    MarcelJflexer jflexer = new MarcelJflexer(content);
+    List<LexToken> tokens = new ArrayList<>();
+    LexToken token;
+    while ((token = jflexer.nextToken()) != null) {
+      tokens.add(token);
+    }
+    return tokens;
+  }
+
 }
