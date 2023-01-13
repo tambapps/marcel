e8d027526b1795f96c83e383fb247640e16e8322
Nelson Fonkoua <nfonkoua@weborama.com>
2023-01-13 17:11:12 +0100

    wrote first class!

--- marcel-compiler/src/main/kotlin/com/tambapps/marcel/compiler/bytecode/BytecodeGenerator.kt
+++ marcel-compiler/src/main/kotlin/com/tambapps/marcel/compiler/bytecode/BytecodeGenerator.kt
@@ -1,19 +1,27 @@
 package com.tambapps.marcel.compiler.bytecode
 
 import com.tambapps.marcel.parser.node.ScriptNode
-import jdk.internal.org.objectweb.asm.ClassWriter
+import org.objectweb.asm.ClassWriter
 import org.objectweb.asm.Opcodes
 
 class BytecodeGenerator {
 
 
   fun generate(node: ScriptNode): ByteArray {
-    val classWriter = ClassWriter(org.objectweb.asm.ClassWriter.COMPUTE_MAXS or org.objectweb.asm.ClassWriter.COMPUTE_FRAMES)
+    val classWriter = ClassWriter(ClassWriter.COMPUTE_MAXS or ClassWriter.COMPUTE_FRAMES)
 
     // TODO make java class version configurable https://www.baeldung.com/java-find-class-version
     classWriter.visit(52,  Opcodes.ACC_PUBLIC + Opcodes.ACC_SUPER, node.className, null, "java/lang/Object", null)
     // TODO generate main function
     //https://github.com/JakubDziworski/Enkel-JVM-language/blob/master/compiler/src/main/java/com/kubadziworski/bytecodegeneration/MethodGenerator.java
+
+    val mv = classWriter.visitMethod(Opcodes.ACC_PUBLIC + Opcodes.ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null)
+    val maxStack = 100; //TODO - do that properly
+    val localVariablesCount = 0 // TODO
+    // TODO write instruction
+    mv.visitInsn(Opcodes.RETURN)
+    mv.visitMaxs(maxStack, localVariablesCount) //set max stack and max local variables
+    mv.visitEnd()
 
     classWriter.visitEnd()
     return classWriter.toByteArray()
--- marcel-compiler/src/test/kotlin/com/tambapps/marcel/compiler/MarcelCompilerTest.kt
+++ marcel-compiler/src/test/kotlin/com/tambapps/marcel/compiler/MarcelCompilerTest.kt
@@ -1,10 +1,21 @@
 package com.tambapps.marcel.compiler
 
+import com.tambapps.marcel.compiler.bytecode.BytecodeGenerator
+import com.tambapps.marcel.parser.node.ScriptNode
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import java.nio.file.Files
+import java.nio.file.Paths
 
 class MarcelCompilerTest {
 
+    @Disabled
     @Test
     fun test() {
+        val name = "Test"
+        val node = ScriptNode(name, mutableListOf())
+        val bytecodeGenerator = BytecodeGenerator()
+
+        Files.write(Paths.get("/home/nfonkoua/Downloads/marcel/$name.class"), bytecodeGenerator.generate(node))
     }
 }
