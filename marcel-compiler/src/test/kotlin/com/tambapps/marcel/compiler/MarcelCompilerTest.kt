9d716857ff326ed29c804a2aacd476f52ab2b76d
Nelson Fonkoua <tambue@hotmail.fr>
2023-01-18 20:06:54 +0100

    another test

--- marcel-compiler/src/test/kotlin/com/tambapps/marcel/compiler/MarcelCompilerTest.kt
+++ marcel-compiler/src/test/kotlin/com/tambapps/marcel/compiler/MarcelCompilerTest.kt
@@ -3,31 +3,40 @@ package com.tambapps.marcel.compiler
 import com.tambapps.marcel.parser.type.JavaType
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
 import java.net.URLClassLoader
 import java.nio.file.Files
 
 class MarcelCompilerTest {
 
-    private val compiler = MarcelCompiler()
-    @Test
-    fun testScript() {
-        val eval = eval("/test1.marcel")
-        assertNotNull(eval)
-        assertEquals(JavaType.OBJECT.realClassOrObject, eval.javaClass)
+  private val compiler = MarcelCompiler()
+
+  @Test
+  fun testScript() {
+    val eval = eval("/test1.marcel")!!
+    assertNotNull(eval)
+    assertEquals(JavaType.OBJECT.realClassOrObject, eval.javaClass)
+  }
+
+  @Test
+  fun testReturnNull() {
+    val eval = eval("/test_return_null.marcel")
+    assertNull(eval)
+  }
+
+
+  private fun eval(resourceName: String): Any? {
+    val result = javaClass.getResourceAsStream(resourceName).reader().use {
+      compiler.compile(it, "Test")
     }
 
+    val jarFile = Files.createTempFile("", "${result.className}.jar").toFile()
+    JarWriter().writeScriptJar(result.className, result.bytes, jarFile)
 
-    private fun eval(resourceName: String): Any {
-        val result = javaClass.getResourceAsStream(resourceName).reader().use {
-            compiler.compile(it, "Test")
-        }
-
-        val jarFile = Files.createTempFile("", "${result.className}.jar").toFile()
-        JarWriter().writeScriptJar(result.className, result.bytes, jarFile)
-
-        val classLoader = URLClassLoader(arrayOf(jarFile.toURI().toURL()), MarcelCompiler::class.java.classLoader)
-        val clazz = classLoader.loadClass(result.className)
-        return clazz.getMethod("run", Array<String>::class.java).invoke(clazz.getDeclaredConstructor().newInstance(), arrayOf<String>())
-    }
+    val classLoader = URLClassLoader(arrayOf(jarFile.toURI().toURL()), MarcelCompiler::class.java.classLoader)
+    val clazz = classLoader.loadClass(result.className)
+    return clazz.getMethod("run", Array<String>::class.java)
+      .invoke(clazz.getDeclaredConstructor().newInstance(), arrayOf<String>())
+  }
 }
