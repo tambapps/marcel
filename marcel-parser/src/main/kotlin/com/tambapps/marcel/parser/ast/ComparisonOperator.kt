b838bb99f11342a64b8614b468a3b40d8f953628
Nelson Fonkoua <tambue@hotmail.fr>
2023-01-19 01:09:57 +0100

    handled comparison operator

---
+++ marcel-parser/src/main/kotlin/com/tambapps/marcel/parser/ast/ComparisonOperator.kt
@@ -0,0 +1,23 @@
+package com.tambapps.marcel.parser.ast
+
+import com.tambapps.marcel.lexer.TokenType
+import com.tambapps.marcel.parser.MarcelParsingException
+import org.objectweb.asm.Opcodes
+
+enum class ComparisonOperator(val tokenType: TokenType, val iOpCode: Int) {
+  EQUAL(TokenType.EQUAL, Opcodes.IF_ICMPEQ),
+  NOT_EQUAL(TokenType.NOT_EQUAL, Opcodes.IF_ICMPNE),
+  LT(TokenType.LT,Opcodes.IF_ICMPLT),
+  GT(TokenType.GT,Opcodes.IF_ICMPGT),
+  LOE(TokenType.LOE,Opcodes.IF_ICMPLE),
+  GOE(TokenType.GOE,Opcodes.IF_ICMPGE);
+
+  companion object {
+
+    fun fromTokenType(tokenType: TokenType): ComparisonOperator {
+      return values().find { it.tokenType == tokenType }
+        ?: throw MarcelParsingException("Invalid comparison operator token $tokenType")
+    }
+  }
+
+}
