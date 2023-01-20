60dbd893fa20f468838b592b70d749c7087fd086
Nelson Fonkoua <nfonkoua@weborama.com>
2023-01-20 15:09:31 +0100

    new day, new equals method

--- marcel-parser/src/main/kotlin/com/tambapps/marcel/parser/type/JavaType.kt
+++ marcel-parser/src/main/kotlin/com/tambapps/marcel/parser/type/JavaType.kt
@@ -73,21 +73,28 @@ open class JavaType(
     val otherClass = Class.forName(javaType.className)
     return thisClass.isAssignableFrom(otherClass)
   }
-  override fun equals(other: Any?): Boolean {
-    if (this === other) return true
-    if (other !is JavaType) return false
-    return className == other.className
-  }
-
-  override fun hashCode(): Int {
-    return className.hashCode()
-  }
 
   override fun toString(): String {
     if (genericTypes.isNotEmpty()) {
       return className + "<" + genericTypes.joinToString(separator = ", ") + ">"
     }
     return className
+  }
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (other !is JavaType) return false
+
+    if (className != other.className) return false
+    if (genericTypes != other.genericTypes) return false
+
+    return true
+  }
+
+  override fun hashCode(): Int {
+    var result = className.hashCode()
+    result = 31 * result + genericTypes.hashCode()
+    return result
   }
 
 }
