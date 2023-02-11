71e0853f9e4c22eeb3f85005df8286bf2d793ed8
Nelson Fonkoua <tambue@hotmail.fr>
2023-02-11 11:39:45 +0100

    more truth

--- marcel-stdlib/src/main/java/marcel/lang/methods/MarcelTruth.java
+++ marcel-stdlib/src/main/java/marcel/lang/methods/MarcelTruth.java
@@ -4,7 +4,11 @@ import java.lang.reflect.Array;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Collection;
+import java.util.Map;
 import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+import java.util.OptionalLong;
 
 public class MarcelTruth {
 
@@ -14,9 +18,9 @@ public class MarcelTruth {
     }
     Class<?> clazz = o.getClass();
     if (clazz == Optional.class) {
-      return ((Optional<?>) o).isPresent();
+      return truthy((Optional<?>) o);
     } else if (o instanceof Collection) {
-      return !((Collection<?>)o).isEmpty();
+      return truthy((Collection<?>) o);
     } else if (clazz.isArray()) {
       return Array.getLength(o) > 0;
     } else {
@@ -24,7 +28,7 @@ public class MarcelTruth {
         Method truthyMethod = clazz.getDeclaredMethod("truthy");
         if (truthyMethod.getReturnType() == boolean.class) {
           return (boolean) truthyMethod.invoke(o);
-        } else  if (truthyMethod.getReturnType() == Boolean.class) {
+        } else if (truthyMethod.getReturnType() == Boolean.class) {
           return (boolean) (Boolean) truthyMethod.invoke(o);
         } else {
           return true;
@@ -33,5 +37,51 @@ public class MarcelTruth {
         return true;
       }
     }
+  }
+
+
+  public static boolean truthy(Collection<?> collection) {
+    return !collection.isEmpty();
+  }
+
+  public static boolean truthy(Map<?, ?> map) {
+    return !map.isEmpty();
+  }
+
+  public static boolean truthy(Optional<?> optional) {
+    return optional.isPresent();
+  }
+  public static boolean truthy(OptionalInt optional) {
+    return optional.isPresent();
+  }
+  public static boolean truthy(OptionalDouble optional) {
+    return optional.isPresent();
+  }
+  public static boolean truthy(OptionalLong optional) {
+    return optional.isPresent();
+  }
+  public static boolean truthy(int value) {
+    return true;
+  }
+  public static boolean truthy(long value) {
+    return true;
+  }
+  public static boolean truthy(float value) {
+    return true;
+  }
+  public static boolean truthy(double value) {
+    return true;
+  }
+  public static boolean truthy(char value) {
+    return true;
+  }
+  public static boolean truthy(short value) {
+    return true;
+  }
+  public static boolean truthy(byte value) {
+    return true;
+  }
+  public static boolean truthy(boolean value) {
+    return true;
   }
 }
