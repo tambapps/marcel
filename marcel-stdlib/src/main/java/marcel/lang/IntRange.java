d43a0bc36048687006b6c081221a6759cd9c1914
Nelson Fonkoua <tambue@hotmail.fr>
2023-02-20 19:32:32 +0100

    ranges have now new methods

--- marcel-stdlib/src/main/java/marcel/lang/IntRange.java
+++ marcel-stdlib/src/main/java/marcel/lang/IntRange.java
@@ -27,6 +27,39 @@ public interface IntRange extends IntIterable {
     return list;
   }
 
+  /**
+   * Returns true if all elements in the other ranges also belong to this range
+   *
+   * @param other the other range
+   * @return true if all elements in the other ranges also belong to this range
+   */
+  default boolean contains(IntRange other) {
+    if (other.isEmpty()) return true;
+    if (isEmpty()) return false;
+    // ranges may not be in ascending order
+    int selfMin = Math.min(getFrom(), getTo());
+    int selfMax = Math.max(getFrom(), getTo());
+    int otherMin = Math.min(other.getFrom(), other.getTo());
+    int otherMax = Math.max(other.getFrom(), other.getTo());
+    return selfMin <= otherMin && selfMax >= otherMax;
+  }
+
+  /**
+   * Returns true if the two ranges have at least one element in common
+   *
+   * @param other the other range
+   * @return true if the two ranges have at least one element in common
+   */
+  default boolean intersects(IntRange other) {
+    if (isEmpty() || other.isEmpty()) return false;
+    // ranges may not be in ascending order
+    int selfMin = Math.min(getFrom(), getTo());
+    int selfMax = Math.max(getFrom(), getTo());
+    int otherMin = Math.min(other.getFrom(), other.getTo());
+    int otherMax = Math.max(other.getFrom(), other.getTo());
+    return selfMin <= otherMax && selfMax >= otherMin;
+  }
+
   int size();
   default int getLength() {
     return size();
--- marcel-stdlib/src/main/java/marcel/lang/LongRange.java
+++ marcel-stdlib/src/main/java/marcel/lang/LongRange.java
@@ -26,6 +26,39 @@ public interface LongRange extends LongIterable {
     return list;
   }
 
+  /**
+   * Returns true if all elements in the other ranges also belong to this range
+   *
+   * @param other the other range
+   * @return true if all elements in the other ranges also belong to this range
+   */
+  default boolean contains(LongRange other) {
+    if (other.isEmpty()) return true;
+    if (isEmpty()) return false;
+    // ranges may not be in ascending order
+    long selfMin = Math.min(getFrom(), getTo());
+    long selfMax = Math.max(getFrom(), getTo());
+    long otherMin = Math.min(other.getFrom(), other.getTo());
+    long otherMax = Math.max(other.getFrom(), other.getTo());
+    return selfMin <= otherMin && selfMax >= otherMax;
+  }
+
+  /**
+   * Returns true if the two ranges have at least one element in common
+   *
+   * @param other the other range
+   * @return true if the two ranges have at least one element in common
+   */
+  default boolean intersects(LongRange other) {
+    if (isEmpty() || other.isEmpty()) return false;
+    // ranges may not be in ascending order
+    long selfMin = Math.min(getFrom(), getTo());
+    long selfMax = Math.max(getFrom(), getTo());
+    long otherMin = Math.min(other.getFrom(), other.getTo());
+    long otherMax = Math.max(other.getFrom(), other.getTo());
+    return selfMin <= otherMax && selfMax >= otherMin;
+  }
+
   int size();
 
   default int getLength() {
