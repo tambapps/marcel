2187e778632da8b735dea1cbdb2275b212b3e72b
Nelson Fonkoua <tambue@hotmail.fr>
2023-02-26 16:13:28 +0100

    put classes in separate jar

--- subprojects/marcel-repl/src/main/kotlin/com/tambapps/marcel/repl/MarcelEvaluator.kt
+++ subprojects/marcel-repl/src/main/kotlin/com/tambapps/marcel/repl/MarcelEvaluator.kt
@@ -14,26 +14,30 @@ class MarcelEvaluator constructor(
   private val tempDir: File
 ) {
 
-  @Throws(MarcelLexerException::class, MarcelParserException::class, MarcelSemanticException::class,
-    ClassNotFoundException::class)
+  @Throws(
+    MarcelLexerException::class, MarcelParserException::class, MarcelSemanticException::class,
+    ClassNotFoundException::class
+  )
   fun eval(code: String): Any? {
     val result = replCompiler.compile(code)
     val scriptNode = result.parserResult.scriptNode
 
     val className = scriptNode.type.simpleName
     val jarFile = File(tempDir.parentFile, "$className.jar")
+
+    if (result.otherClasses.isNotEmpty()) {
+      val libraryJar = File(tempDir.parentFile, "${className}_library.jar")
+      JarWriter(libraryJar).use {
+        it.writeScriptJar(result.otherClasses)
+      }
+      scriptLoader.addLibraryJar(libraryJar)
+    }
     JarWriter(jarFile).use {
-      it.writeScriptJar(result.compiledScript + result.otherClasses)
+      it.writeScriptJar(result.compiledScript)
     }
     if (scriptNode.fields.isNotEmpty()) {
       throw MarcelSemanticException("Cannot define field variables in Marshell. Use global or local variables only")
     }
-    val eval = scriptLoader.loadScript(className, jarFile, binding).run()
-    if (result.otherClasses.isNotEmpty()) {
-      // TODO need separate jar for library
-      // some other classes were defined? need to keep jar for future evals.
-      scriptLoader.addLibraryJar(jarFile)
-    }
-    return eval
+    return scriptLoader.loadScript(className, jarFile, binding).run()
   }
 }
