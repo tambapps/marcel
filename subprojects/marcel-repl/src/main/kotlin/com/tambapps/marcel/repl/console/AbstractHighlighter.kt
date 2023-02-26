38b117454649141c12b6dbef0e6df71021263512
Nelson Fonkoua <tambue@hotmail.fr>
2023-02-26 01:11:48 +0100

    abstract highlighter

---
+++ subprojects/marcel-repl/src/main/kotlin/com/tambapps/marcel/repl/console/AbstractHighlighter.kt
@@ -0,0 +1,96 @@
+package com.tambapps.marcel.repl.console
+
+import com.tambapps.marcel.compiler.JavaTypeResolver
+import com.tambapps.marcel.lexer.LexToken
+import com.tambapps.marcel.lexer.MarcelLexer
+import com.tambapps.marcel.lexer.TokenType.*
+import com.tambapps.marcel.repl.MarcelReplCompiler
+import com.tambapps.marcel.parser.ast.MethodNode
+import com.tambapps.marcel.parser.ast.ScopedNode
+import com.tambapps.marcel.parser.ast.expression.FunctionCallNode
+import com.tambapps.marcel.parser.ast.expression.IndexedReferenceExpression
+import com.tambapps.marcel.parser.ast.expression.IndexedVariableAssignmentNode
+import com.tambapps.marcel.parser.ast.expression.ReferenceExpression
+import com.tambapps.marcel.parser.ast.expression.VariableAssignmentNode
+import com.tambapps.marcel.parser.exception.MarcelSemanticException
+import java.util.regex.Pattern
+
+abstract class AbstractHighlighter<T, Style> constructor(
+  private val typeResolver: JavaTypeResolver,
+  private val replCompiler: MarcelReplCompiler
+) {
+
+  private val lexer = MarcelLexer(false)
+  
+  abstract val variableStyle: Style
+  abstract val functionStyle: Style
+  abstract val stringStyle: Style
+  abstract val stringTemplateStyle: Style
+  abstract val keywordStyle: Style
+  abstract val commentStyle: Style
+  abstract val numberStyle: Style
+  abstract val defaultStyle: Style
+
+  abstract fun newHighlightedString(): T
+
+  fun highlightt(text: String): T {
+    val highlightedString = newHighlightedString()
+    val parseResult = replCompiler.tryParse(text)
+    val tokens = parseResult?.tokens?.toMutableList() ?: lexer.lexSafely(text)
+    tokens.removeLast() // remove end of file
+    val scriptNode = parseResult?.scriptNode ?: replCompiler.parserResult?.scriptNode
+    val node = scriptNode?.methods?.find { it.name == "run" && it.parameters.size == 1 }
+
+    for (token in tokens) {
+      val string = text.substring(token.start, token.end)
+      val style = when (token.type) {
+        IDENTIFIER -> identifierStyle(token, node)
+        TYPE_INT, TYPE_LONG, TYPE_SHORT, TYPE_FLOAT, TYPE_DOUBLE, TYPE_BOOL, TYPE_BYTE, TYPE_VOID, TYPE_CHAR, FUN, RETURN,
+        VALUE_TRUE, VALUE_FALSE, NEW, IMPORT, AS, INLINE, STATIC, FOR, IN, IF, ELSE, NULL, BREAK, CONTINUE, DEF,
+        CLASS, EXTENSION, PACKAGE, EXTENDS, IMPLEMENTS, FINAL, SWITCH, WHEN, THIS, SUPER,
+          // visibilities
+        VISIBILITY_PUBLIC, VISIBILITY_PROTECTED, VISIBILITY_INTERNAL, VISIBILITY_PRIVATE -> keywordStyle
+        INTEGER, FLOAT -> numberStyle
+        BLOCK_COMMENT, DOC_COMMENT, HASH, SHEBANG_COMMENT, EOL_COMMENT -> commentStyle
+        OPEN_QUOTE, CLOSING_QUOTE, REGULAR_STRING_PART,
+        OPEN_CHAR_QUOTE, CLOSING_CHAR_QUOTE,
+        OPEN_REGEX_QUOTE, CLOSING_REGEX_QUOTE,
+        OPEN_SIMPLE_QUOTE, CLOSING_SIMPLE_QUOTE -> stringStyle
+        SHORT_TEMPLATE_ENTRY_START, LONG_TEMPLATE_ENTRY_START, LONG_TEMPLATE_ENTRY_END -> stringTemplateStyle
+        else -> defaultStyle
+      }
+      highlight(highlightedString, style, string)
+    }
+    return highlightedString
+  }
+
+  protected abstract fun highlight(highlightedString: T, style: Style, string: String)
+
+
+  private fun identifierStyle(token: LexToken, scriptNode: MethodNode?): Style {
+    if (scriptNode == null) return defaultStyle
+    val node = scriptNode.block.find {
+      it.token == token && (it is VariableAssignmentNode || it is ReferenceExpression || it is IndexedVariableAssignmentNode
+          || it is IndexedReferenceExpression || it is FunctionCallNode)
+
+    } ?: return defaultStyle
+
+    return when (node) {
+      is VariableAssignmentNode -> variableHighlight(node, node.name)
+      is ReferenceExpression -> variableHighlight(node, node.name)
+      is FunctionCallNode -> {
+        val method = try { node.getMethod(typeResolver) } catch (e: MarcelSemanticException) { null }
+        if (method != null) functionStyle
+        else defaultStyle
+      }
+      else -> defaultStyle
+    }
+  }
+
+  private fun variableHighlight(node: ScopedNode<*>, name: String): Style {
+    val variable = node.scope.findVariable(name)
+    return if (variable != null) variableStyle
+    else defaultStyle
+  }
+
+}
--- subprojects/marshell/src/main/kotlin/com/tambapps/marcel/marshell/console/ReaderHighlighter.kt
+++ subprojects/marshell/src/main/kotlin/com/tambapps/marcel/marshell/console/ReaderHighlighter.kt
@@ -1,19 +1,9 @@
 package com.tambapps.marcel.marshell.console
 
 import com.tambapps.marcel.compiler.JavaTypeResolver
-import com.tambapps.marcel.lexer.LexToken
-import com.tambapps.marcel.lexer.MarcelLexer
-import com.tambapps.marcel.lexer.TokenType.*
 import com.tambapps.marcel.marshell.console.style.HighlightTheme
 import com.tambapps.marcel.repl.MarcelReplCompiler
-import com.tambapps.marcel.parser.ast.MethodNode
-import com.tambapps.marcel.parser.ast.ScopedNode
-import com.tambapps.marcel.parser.ast.expression.FunctionCallNode
-import com.tambapps.marcel.parser.ast.expression.IndexedReferenceExpression
-import com.tambapps.marcel.parser.ast.expression.IndexedVariableAssignmentNode
-import com.tambapps.marcel.parser.ast.expression.ReferenceExpression
-import com.tambapps.marcel.parser.ast.expression.VariableAssignmentNode
-import com.tambapps.marcel.parser.exception.MarcelSemanticException
+import com.tambapps.marcel.repl.console.AbstractHighlighter
 import org.jline.reader.Highlighter
 import org.jline.reader.LineReader
 import org.jline.utils.AttributedString
@@ -22,74 +12,32 @@ import org.jline.utils.AttributedStyle
 import java.util.regex.Pattern
 
 class ReaderHighlighter constructor(
-  private val typeResolver: JavaTypeResolver,
-  private val replCompiler: MarcelReplCompiler
-): Highlighter {
+  typeResolver: JavaTypeResolver,
+  replCompiler: MarcelReplCompiler
+): AbstractHighlighter<AttributedStringBuilder, AttributedStyle>(typeResolver, replCompiler), Highlighter {
 
-  private val lexer = MarcelLexer(false)
   private val style = HighlightTheme()
+  override val variableStyle: AttributedStyle = style.variable
+  override val functionStyle: AttributedStyle = style.function
+  override val stringStyle: AttributedStyle = style.string
+  override val stringTemplateStyle: AttributedStyle = style.stringTemplate
+  override val keywordStyle: AttributedStyle = style.keyword
+  override val commentStyle: AttributedStyle = style.comment
+  override val numberStyle: AttributedStyle = style.number
+  override val defaultStyle: AttributedStyle = AttributedStyle.DEFAULT
 
-  override fun highlight(reader: LineReader, text: String): AttributedString {
-    val highlightedString = AttributedStringBuilder()
-    val parseResult = replCompiler.tryParse(text)
-    val tokens = parseResult?.tokens?.toMutableList() ?: lexer.lexSafely(text)
-    tokens.removeLast() // remove end of file
-    val scriptNode = parseResult?.scriptNode ?: replCompiler.parserResult?.scriptNode
-    val node = scriptNode?.methods?.find { it.name == "run" && it.parameters.size == 1 }
-
-    for (token in tokens) {
-      val string = text.substring(token.start, token.end)
-      val style = when (token.type) {
-        IDENTIFIER -> identifierStyle(token, node)
-        TYPE_INT, TYPE_LONG, TYPE_SHORT, TYPE_FLOAT, TYPE_DOUBLE, TYPE_BOOL, TYPE_BYTE, TYPE_VOID, TYPE_CHAR, FUN, RETURN,
-        VALUE_TRUE, VALUE_FALSE, NEW, IMPORT, AS, INLINE, STATIC, FOR, IN, IF, ELSE, NULL, BREAK, CONTINUE, DEF,
-        CLASS, EXTENSION, PACKAGE, EXTENDS, IMPLEMENTS, FINAL, SWITCH, WHEN, THIS, SUPER,
-          // visibilities
-        VISIBILITY_PUBLIC, VISIBILITY_PROTECTED, VISIBILITY_INTERNAL, VISIBILITY_PRIVATE -> style.keyword
-        INTEGER, FLOAT -> style.number
-        BLOCK_COMMENT, DOC_COMMENT, HASH, SHEBANG_COMMENT, EOL_COMMENT -> style.comment
-        OPEN_QUOTE, CLOSING_QUOTE, REGULAR_STRING_PART,
-        OPEN_CHAR_QUOTE, CLOSING_CHAR_QUOTE,
-        OPEN_REGEX_QUOTE, CLOSING_REGEX_QUOTE,
-        OPEN_SIMPLE_QUOTE, CLOSING_SIMPLE_QUOTE -> style.string
-        SHORT_TEMPLATE_ENTRY_START, LONG_TEMPLATE_ENTRY_START, LONG_TEMPLATE_ENTRY_END -> style.stringTemplate
-        else -> AttributedStyle.DEFAULT
-      }
-      highlight(highlightedString, style, string)
-    }
-    return highlightedString.toAttributedString()
+  override fun highlight(reader: LineReader?, buffer: String): AttributedString {
+    return highlightt(buffer).toAttributedString()
   }
 
-  private fun identifierStyle(token: LexToken, scriptNode: MethodNode?): AttributedStyle {
-    if (scriptNode == null) return AttributedStyle.DEFAULT
-    val node = scriptNode.block.find {
-      it.token == token && (it is VariableAssignmentNode || it is ReferenceExpression || it is IndexedVariableAssignmentNode
-          || it is IndexedReferenceExpression || it is FunctionCallNode)
-
-    } ?: return AttributedStyle.DEFAULT
-
-    return when (node) {
-      is VariableAssignmentNode -> variableHighlight(node, node.name)
-      is ReferenceExpression -> variableHighlight(node, node.name)
-      is FunctionCallNode -> {
-        val method = try { node.getMethod(typeResolver) } catch (e: MarcelSemanticException) { null }
-        if (method != null) style.function
-        else AttributedStyle.DEFAULT
-      }
-      else -> AttributedStyle.DEFAULT
-    }
+  override fun newHighlightedString(): AttributedStringBuilder {
+    return AttributedStringBuilder()
   }
 
-  private fun variableHighlight(node: ScopedNode<*>, name: String): AttributedStyle {
-    val variable = node.scope.findVariable(name)
-    return if (variable != null) style.variable
-    else AttributedStyle.DEFAULT
+  override fun highlight(highlightedString: AttributedStringBuilder, style: AttributedStyle, string: String) {
+    highlightedString.style(style)
+    highlightedString.append(string)
   }
-  private fun highlight(builder: AttributedStringBuilder, style: AttributedStyle, string: String) {
-    builder.style(style)
-    builder.append(string)
-  }
-
 
   override fun setErrorPattern(errorPattern: Pattern) {}
 
