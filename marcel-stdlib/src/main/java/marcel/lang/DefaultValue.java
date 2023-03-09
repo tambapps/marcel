82dd190d5ff8dc4d5a85323772f2485f0a5ae30c
Nelson FONKOUA <nelson.fonkoua@vialink.fr>
2023-03-09 14:24:54 +0100

    need to retain this annotation to runtime

--- marcel-stdlib/src/main/java/marcel/lang/DefaultValue.java
+++ marcel-stdlib/src/main/java/marcel/lang/DefaultValue.java
@@ -1,5 +1,10 @@
 package marcel.lang;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+
 public @interface DefaultValue {
 
   int defaultIntValue() default 0;
