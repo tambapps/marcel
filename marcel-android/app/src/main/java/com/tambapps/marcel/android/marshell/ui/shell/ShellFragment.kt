0166219e667c8b0a05c8b040a8c5df5caa420012
Nelson Fonkoua <tambue@hotmail.fr>
2023-03-25 22:09:53 +0100

    allowed to have multiple shells

--- marcel-android/app/src/main/java/com/tambapps/marcel/android/marshell/ui/shell/ShellFragment.kt
+++ marcel-android/app/src/main/java/com/tambapps/marcel/android/marshell/ui/shell/ShellFragment.kt
@@ -5,25 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.tambapps.marcel.android.marshell.databinding.FragmentShellBinding
-import com.tambapps.marcel.android.marshell.AndroidMarshell
 import com.tambapps.marcel.android.marshell.repl.AndroidMarshellFactory
-import com.tambapps.marcel.android.marshell.util.showSoftBoard
-import com.tambapps.marcel.compiler.CompilerConfiguration
 import dagger.hilt.android.AndroidEntryPoint
-import com.tambapps.marcel.android.marshell.view.EditTextHighlighter
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
-import marcel.lang.MarcelSystem
-import marcel.lang.MarcelDexClassLoader
-import java.io.File
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
-import java.util.concurrent.LinkedBlockingQueue
 import javax.inject.Inject
-import javax.inject.Named
-
 
 @AndroidEntryPoint
 class ShellFragment : Fragment() {
@@ -33,11 +19,6 @@ class ShellFragment : Fragment() {
   lateinit var factory: AndroidMarshellFactory
   private val binding get() = _binding!!
 
-  private lateinit var marshell: AndroidMarshell
-  private lateinit var printer: TextViewPrinter
-  private lateinit var editTextHighlighter: EditTextHighlighter
-  private lateinit var executor: ExecutorService
-  private lateinit var promptQueue: LinkedBlockingQueue<CharSequence>
 
   override fun onCreateView(
     inflater: LayoutInflater,
@@ -49,63 +30,23 @@ class ShellFragment : Fragment() {
   }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    executor = Executors.newSingleThreadExecutor()
-    promptQueue = LinkedBlockingQueue<CharSequence>()
-
-    printer = TextViewPrinter(requireActivity(), binding.historyText)
-    marshell = factory.newShell(printer, this::readLine)
-    val highlighter = marshell.newHighlighter()
-    editTextHighlighter = EditTextHighlighter(binding.promptEditText, highlighter)
-
-    executor.submit {
-      runBlocking {
-        marshell.printVersion()
-        marshell.run()
-      }
-    }
-    binding.apply {
-      promptEditText.setOnKeyListener(PromptKeyListener(promptQueue))
-      promptEditText.setOnFocusChangeListener { v, hasFocus ->
-        if (hasFocus) {
-          requireContext().showSoftBoard(v)
-        }
-      }
-      promptEditText.requestFocus()
-      historyText.setOnClickListener {
-        promptEditText.requestFocus()
-      }
-    }
+    binding.viewPager.adapter = ShellWidowStateAdapter(this)
   }
 
-  override fun onStart() {
-    // TODO move this kind of thing in a MarcelEngine class
-    super.onStart()
-    MarcelSystem.setPrinter(printer)
-    editTextHighlighter.start()
-  }
   override fun onDestroyView() {
     super.onDestroyView()
-    runBlocking { marshell.exit() }
     _binding = null
-    executor.shutdown()
   }
 
-  override fun onStop() {
-    super.onStop()
-    // TODO move this kind of thing in a MarcelEngine class
-    MarcelSystem.setPrinter(null)
-    editTextHighlighter.cancel()
-  }
-  private suspend fun readLine(prompt: String): String {
-    withContext(Dispatchers.Main) {
-      binding.historyText.append(prompt)
-      binding.fakePromptText.text = prompt
+  private class ShellWidowStateAdapter(fragment: Fragment): FragmentStateAdapter(fragment) {
+    override fun getItemCount(): Int {
+      // TODO make tab layout and everything
+      return 2
     }
-    val text = promptQueue.take()
-    withContext(Dispatchers.Main) {
-      printer.println(text)
-      binding.promptEditText.setText("")
+
+    override fun createFragment(position: Int): Fragment {
+      return ShellWindowFragment.newInstance()
     }
-    return text.toString()
+
   }
 }
---
+++ marcel-android/app/src/main/java/com/tambapps/marcel/android/marshell/ui/shell/ShellWindowFragment.kt
@@ -0,0 +1,110 @@
+package com.tambapps.marcel.android.marshell.ui.shell
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.tambapps.marcel.android.marshell.AndroidMarshell
+import com.tambapps.marcel.android.marshell.databinding.FragmentShellWindowBinding
+import com.tambapps.marcel.android.marshell.repl.AndroidMarshellFactory
+import com.tambapps.marcel.android.marshell.util.showSoftBoard
+import dagger.hilt.android.AndroidEntryPoint
+import com.tambapps.marcel.android.marshell.view.EditTextHighlighter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import marcel.lang.MarcelSystem
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.LinkedBlockingQueue
+import javax.inject.Inject
+
+
+@AndroidEntryPoint
+class ShellWindowFragment : Fragment() {
+
+  companion object {
+    fun newInstance() = ShellWindowFragment()
+  }
+  private var _binding: FragmentShellWindowBinding? = null
+  @Inject
+  lateinit var factory: AndroidMarshellFactory
+  private val binding get() = _binding!!
+
+  private lateinit var marshell: AndroidMarshell
+  private lateinit var printer: TextViewPrinter
+  private lateinit var editTextHighlighter: EditTextHighlighter
+  private lateinit var executor: ExecutorService
+  private lateinit var promptQueue: LinkedBlockingQueue<CharSequence>
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    _binding = FragmentShellWindowBinding.inflate(inflater, container, false)
+    return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    executor = Executors.newSingleThreadExecutor()
+    promptQueue = LinkedBlockingQueue<CharSequence>()
+
+    printer = TextViewPrinter(requireActivity(), binding.historyText)
+    marshell = factory.newShell(printer, this::readLine)
+    val highlighter = marshell.newHighlighter()
+    editTextHighlighter = EditTextHighlighter(binding.promptEditText, highlighter)
+
+    executor.submit {
+      runBlocking {
+        marshell.printVersion()
+        marshell.run()
+      }
+    }
+    binding.apply {
+      promptEditText.setOnKeyListener(PromptKeyListener(promptQueue))
+      promptEditText.setOnFocusChangeListener { v, hasFocus ->
+        if (hasFocus) {
+          requireContext().showSoftBoard(v)
+        }
+      }
+      promptEditText.requestFocus()
+      historyText.setOnClickListener {
+        promptEditText.requestFocus()
+      }
+    }
+  }
+
+  override fun onStart() {
+    // TODO move this kind of thing in a MarcelEngine class
+    super.onStart()
+    MarcelSystem.setPrinter(printer)
+    editTextHighlighter.start()
+  }
+  override fun onDestroyView() {
+    super.onDestroyView()
+    runBlocking { marshell.exit() }
+    _binding = null
+    executor.shutdown()
+  }
+
+  override fun onStop() {
+    super.onStop()
+    // TODO move this kind of thing in a MarcelEngine class
+    MarcelSystem.setPrinter(null)
+    editTextHighlighter.cancel()
+  }
+  private suspend fun readLine(prompt: String): String {
+    withContext(Dispatchers.Main) {
+      binding.historyText.append(prompt)
+      binding.fakePromptText.text = prompt
+    }
+    val text = promptQueue.take()
+    withContext(Dispatchers.Main) {
+      printer.println(text)
+      binding.promptEditText.setText("")
+    }
+    return text.toString()
+  }
+}
