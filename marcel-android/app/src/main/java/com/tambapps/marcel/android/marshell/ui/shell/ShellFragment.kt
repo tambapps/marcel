5f9f52ad31ca7feb0b7264039202440a3e35db67
Nelson Fonkoua <tambue@hotmail.fr>
2023-04-02 00:24:57 +0200

    set up a tablayout for different sessions

--- marcel-android/app/src/main/java/com/tambapps/marcel/android/marshell/ui/shell/ShellFragment.kt
+++ marcel-android/app/src/main/java/com/tambapps/marcel/android/marshell/ui/shell/ShellFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.tambapps.marcel.android.marshell.ShellHandler
 import com.tambapps.marcel.android.marshell.data.ShellSession
 import com.tambapps.marcel.android.marshell.databinding.FragmentShellBinding
@@ -14,7 +16,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class ShellFragment : Fragment() {
+class ShellFragment : Fragment(), TabLayoutMediator.TabConfigurationStrategy {
 
   private var _binding: FragmentShellBinding? = null
   private val binding get() = _binding!!
@@ -36,15 +38,24 @@ class ShellFragment : Fragment() {
     shellHandler = requireActivity() as ShellHandler
     val adapter = ShellWidowStateAdapter(this, shellHandler.shellSessions)
     binding.viewPager.adapter = adapter
+    // setup tabLayout with viewPage
+    TabLayoutMediator(binding.tabLayout, binding.viewPager, this).attach()
+
     binding.plusButton.setOnClickListener {
       if (shellHandler.startNewSession()) {
         val position = shellHandler.shellSessions.size - 1
         adapter.notifyItemInserted(position)
-        binding.viewPager.currentItem = position
+        binding.tabLayout.getTabAt(position)?.select()
+        binding.tabLayout.visibility = View.VISIBLE
       }
     }
+    binding.tabLayout.visibility = if (shellHandler.shellSessions.size > 1) View.VISIBLE else View.GONE
   }
 
+  override fun onConfigureTab(tab: TabLayout.Tab, position: Int) {
+    // TODO generate name and save it in sessions, so that it doesn't change when removing a session
+    tab.text = "Session $position"
+  }
   override fun onDestroyView() {
     super.onDestroyView()
     _binding = null
