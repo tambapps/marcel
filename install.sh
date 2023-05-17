d66d14793384d407f7d7d4d9cec82ee92ec982ef
Nelson FONKOUA <nelson.fonkoua@vialink.fr>
2023-05-17 16:54:32 +0200

    oopsy

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ mkdir -p $marcelDir/bin
 version=$(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout)
 
 echo "Installing marcel..."
-#mvn clean install
+mvn clean install
 cp marcel-lexer/target/marcel-lexer-$version.jar $marcelDir/lib/marcel-lexer.jar
 cp marcel-stdlib/target/marcel-stdlib-$version.jar $marcelDir/lib/marcel-stdlib.jar
 cp marcel-parser/target/marcel-parser-$version.jar $marcelDir/lib/marcel-parser.jar
