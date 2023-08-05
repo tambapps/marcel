9a231af51d0386a02bc853537740a0a406cf83e7
Nelson Fonkoua <tambue@hotmail.fr>
2023-08-06 00:21:14 +0200

    fixed DelegateAnnotationProcessor not taking into account the field it was annotated in

--- marcel-compiler/src/main/kotlin/com/tambapps/marcel/compiler/annotation/DelegateAnnotationProcessor.kt
+++ marcel-compiler/src/main/kotlin/com/tambapps/marcel/compiler/annotation/DelegateAnnotationProcessor.kt
@@ -17,20 +17,17 @@ class DelegateAnnotationProcessor: FieldAnnotationProcessor {
         if (!classNode.type.implements(DelegatedObject::class.javaType)) {
             classNode.type.addImplementedInterface(DelegatedObject::class.javaType.withGenericTypes(fieldNode.type.objectType))
         }
-        if (classNode.type.implements(DelegatedObject::class.java.javaType)
-            && !classNode.methods.any { it.name == "getDelegate" && it.parameters.isEmpty() }
-            && classNode.fields.any { it.name == "delegate" }) {
-            val token = classNode.fields.find { it.name == "delegate" }!!.token
-            val delegateField = classNode.fields.find { it.name == "delegate" }!!
-            if (classNode.type.allImplementedInterfaces.find { it.raw() == DelegatedObject::class.javaType }!!.genericTypes.firstOrNull()?.let { it.isAssignableFrom(delegateField.type) } != false) {
+        if (!classNode.methods.any { it.name == "getDelegate" && it.parameters.isEmpty() }) {
+            val token = fieldNode.token
+            if (classNode.type.allImplementedInterfaces.find { it.raw() == DelegatedObject::class.javaType }!!.genericTypes.firstOrNull()?.let { it.isAssignableFrom(fieldNode.type) } != false) {
                 val getDelegateMethod = MethodNode.from(classNode.token, classScope = classNode.scope, ownerClass = classNode.type, name = "getDelegate", parameters = emptyList(),
-                    returnType = delegateField.type.objectType, annotations = listOf(AnnotationNode(token, Override::class.javaAnnotation, emptyList())), staticContext = false
+                    returnType = fieldNode.type.objectType, annotations = listOf(AnnotationNode(token, Override::class.javaAnnotation, emptyList())), staticContext = false
                 )
                 val scope = getDelegateMethod.scope
                 getDelegateMethod.block.addStatement(
                     ReturnNode(
                             token, scope, GetFieldAccessOperator(
-                            token, ReferenceExpression.thisRef(scope), ReferenceExpression(token, scope, "delegate"), nullSafe = false, directFieldAccess = true)))
+                            token, ReferenceExpression.thisRef(scope), ReferenceExpression(token, scope, fieldNode.name), nullSafe = false, directFieldAccess = true)))
                 classNode.addMethod(getDelegateMethod)
             }
         }
