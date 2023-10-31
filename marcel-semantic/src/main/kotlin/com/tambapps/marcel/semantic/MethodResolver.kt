a0e846dfa9607ed611ba327592b1f050c99fc112
Nelson Fonkoua <tambue@hotmail.fr>
2023-10-31 08:45:09 +0100

    started handle method with named parameters

--- marcel-semantic/src/main/kotlin/com/tambapps/marcel/semantic/MethodResolver.kt
+++ marcel-semantic/src/main/kotlin/com/tambapps/marcel/semantic/MethodResolver.kt
@@ -3,6 +3,7 @@ package com.tambapps.marcel.semantic
 import com.tambapps.marcel.semantic.ast.ImportNode
 import com.tambapps.marcel.semantic.ast.expression.ExpressionNode
 import com.tambapps.marcel.semantic.method.JavaMethod
+import com.tambapps.marcel.semantic.method.MethodParameter
 import com.tambapps.marcel.semantic.type.JavaType
 import com.tambapps.marcel.semantic.type.JavaTypeResolver
 
@@ -28,9 +29,22 @@ internal class MethodResolver(
       }
       return method?.let { Pair(it, positionalArguments) }
     }
+    val namedMethodParameters = namedArguments.map { MethodParameter(it.second.type, it.first) }
+    var method = typeResolver.findMethodByParameters(ownerType, name, positionalArguments, namedMethodParameters)
+    if (method == null && lookupImports) {
+      // then search on (static) imports
+      method = null // TODO resolve with named parameters
 
-
-    TODO()
+    }
+    /*
+    val method = getMethod(typeResolver)
+    return positionalArguments + method.parameters.subList(positionalArguments.size, method.parameters.size).map { parameter: MethodParameter ->
+      namedArguments.find { it.name  == parameter.name }?.valueExpression
+        ?: parameter.defaultValue
+        ?: parameter.type.defaultValueExpression
+    }
+     */
+    return TODO()
   }
 
 }
