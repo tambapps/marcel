f21501cb7d53ed41da844a4c5413337fd81a90db
Nelson Fonkoua <tambapps@gmail.com>
2023-11-19 19:27:54 +0100

    completed description

--- marcel-stdlib/src/main/java/marcel/lang/Delegate.java
+++ marcel-stdlib/src/main/java/marcel/lang/Delegate.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.FIELD;
 
 /**
- * Interface used to indicate
+ * Interface used to indicate that the annotated field should be a delegate of the class
  */
 @Target(value={FIELD})
 @Retention(RetentionPolicy.RUNTIME)
