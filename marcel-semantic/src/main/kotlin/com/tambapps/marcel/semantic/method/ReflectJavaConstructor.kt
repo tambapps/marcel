efbfcbe67948fafb93f40df5f425ec4642a15400
Nelson Fonkoua <tambapps@gmail.com>
2023-11-25 22:58:06 +0100

    handled method default parameters on cnostructors

--- marcel-semantic/src/main/kotlin/com/tambapps/marcel/semantic/method/ReflectJavaConstructor.kt
+++ marcel-semantic/src/main/kotlin/com/tambapps/marcel/semantic/method/ReflectJavaConstructor.kt
@@ -6,7 +6,7 @@ import java.lang.reflect.Constructor
 
 class ReflectJavaConstructor(constructor: Constructor<*>): AbstractConstructor(
   JavaType.of(constructor.declaringClass),
-  constructor.parameters.map { MethodParameter(JavaType.of(it.type), it.name) }
+  constructor.parameters.map { ReflectJavaMethod.methodParameter(constructor.name, JavaType.of(constructor.declaringClass), null, it) }
 ) {
   override val visibility = Visibility.fromAccess(constructor.modifiers)
   override fun toString(): String {
