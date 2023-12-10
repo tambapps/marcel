c762aa5c5b630f7b06904c9f18f901c78ada926a
Nelson Fonkoua <tambapps@gmail.com>
2023-12-10 20:28:55 +0100

    tested AOC 2023

--- marcl/src/test/kotlin/com/tambapps/marcel/cl/AdventOfCodeTest.kt
+++ marcl/src/test/kotlin/com/tambapps/marcel/cl/AdventOfCodeTest.kt
@@ -11,7 +11,7 @@ import java.net.URL
 class AdventOfCodeTest: AbstractCompilerTest() {
 
   companion object {
-    const val URL_TEMPLATE = "https://raw.githubusercontent.com/tambapps/advent-of-code/main/2022/%02d/%s"
+    const val URL_TEMPLATE = "https://raw.githubusercontent.com/tambapps/advent-of-code/main/%d/%02d/%s"
 
     @JvmStatic
     @AfterAll
@@ -20,11 +20,17 @@ class AdventOfCodeTest: AbstractCompilerTest() {
     }
   }
 
-  @ParameterizedTest(name = "AOC day {arguments}")
+  @ParameterizedTest(name = "AOC 2022 day {arguments}")
   @ValueSource(ints = [1, 2, 3, 4, 6, 7])
-  fun aocDay(day: Int) {
-    File("input.txt").writeText(URL(URL_TEMPLATE.format(day, "input.txt")).readText())
-    val text = URL(URL_TEMPLATE.format(day, "solution.mcl")).readText()
+  fun aocDay_2022(day: Int) = aocDay(2022, day)
+
+  @ParameterizedTest(name = "AOC 2023 day {arguments}")
+  @ValueSource(ints = [1, 2, 3, 4, 6, 7, 8, 9, 10])
+  fun aocDay_2023(day: Int) = aocDay(2023, day)
+
+  fun aocDay(year: Int, day: Int) {
+    File("input.txt").writeText(URL(URL_TEMPLATE.format(year, day, "input.txt")).readText())
+    val text = URL(URL_TEMPLATE.format(year, day, "solution.mcl")).readText()
     evalSource("AOC$day", text)
   }
 }
