a9808184be17f9ec301e91257097325f86065da8
Nelson Fonkoua <tambapps@gmail.com>
2023-12-24 00:25:40 +0100

    code style

--- subprojects/marcel-repl/src/main/kotlin/com/tambapps/marcel/repl/printer/PrintStreamPrinter.kt
+++ subprojects/marcel-repl/src/main/kotlin/com/tambapps/marcel/repl/printer/PrintStreamPrinter.kt
@@ -4,15 +4,9 @@ import java.io.PrintStream
 
 class PrintStreamPrinter(private val printStream: PrintStream): Printer {
 
-  override suspend fun print(s: CharSequence?) {
-    printStream.print(s)
-  }
+  override suspend fun print(s: CharSequence?) = printStream.print(s)
 
-  override suspend fun println(s: CharSequence?) {
-    printStream.println(s)
-  }
+  override suspend fun println(s: CharSequence?) = printStream.println(s)
 
-  override suspend fun println() {
-    printStream.println()
-  }
+  override suspend fun println() = printStream.println()
 }
