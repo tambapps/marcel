d5c4e579b575f40e666ab3d9150b82c2958c4a9b
Nelson Fonkoua <tambapps@gmail.com>
2024-02-02 00:40:12 +0100

    cleaning

--- subprojects/marcel-yaml/src/main/java/marcel/yaml/YamlSlurper.java
+++ subprojects/marcel-yaml/src/main/java/marcel/yaml/YamlSlurper.java
@@ -1,6 +1,6 @@
 package marcel.yaml;
 
-
+import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
@@ -10,8 +10,13 @@ import marcel.json.JsonSlurper;
 public class YamlSlurper extends JsonSlurper {
 
   public YamlSlurper() {
-    this(newObjectMapper(new ObjectMapper(new YAMLFactory().disable(YAMLGenerator.Feature.WRITE_DOC_START_MARKER))));
+    this(newObjectMapper(new ObjectMapper(newYamlFactory())));
     mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+  }
+
+  private static JsonFactory newYamlFactory() {
+    return new YAMLFactory()
+        .disable(YAMLGenerator.Feature.WRITE_DOC_START_MARKER);
   }
 
   public YamlSlurper(ObjectMapper mapper) {
