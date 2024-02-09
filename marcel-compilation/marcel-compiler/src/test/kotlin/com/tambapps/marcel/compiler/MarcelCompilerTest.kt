29f2a6145da73e8af228ff1aeff99d9da8f3447e
Nelson FONKOUA <nelson.fonkoua@vialink.fr>
2024-02-09 20:31:22 +0100

    added and fixed test

--- marcel-compilation/marcel-compiler/src/test/kotlin/com/tambapps/marcel/compiler/MarcelCompilerTest.kt
+++ marcel-compilation/marcel-compiler/src/test/kotlin/com/tambapps/marcel/compiler/MarcelCompilerTest.kt
@@ -93,7 +93,7 @@ class MarcelCompilerTest: AbstractCompilerTest() {
   @Disabled
   @Test
   fun manualTest() {
-    val eval = eval("/tests/test_delegable.mcl")
+    val eval = eval("/tests/test_imports.mcl")
     println(eval)
   }
 }
