f76214af630662bff8bbe0c6a5d685644c583ee3
Nelson Fonkoua <tambapps@gmail.com>
2024-04-19 19:49:11 +0900

    reviewed design

--- marcel-android/app/src/main/java/com/tambapps/marcel/android/marshell/ShellScreen.kt
+++ marcel-android/app/src/main/java/com/tambapps/marcel/android/marshell/ShellScreen.kt
@@ -5,42 +5,53 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.tambapps.marcel.android.marshell.ui.model.Prompt
 import com.tambapps.marcel.android.marshell.ui.theme.shellTextStyle
 import com.tambapps.marcel.android.marshell.ui.viewmodel.ShellViewModel
 
 @Composable
 fun ShellScreen(viewModel: ShellViewModel = viewModel()) {
   Column(modifier = Modifier.fillMaxSize()) {
-    Row(modifier = Modifier.fillMaxWidth()) {
-      Text(
-        text = "000>",
-        style = shellTextStyle
-      )
+    LazyColumn {
+      viewModel.prompts.forEach {  prompt: Prompt ->
+        if (prompt.input != null) {
+          item {
+            Text(
+              text = prompt.input,
+              style = shellTextStyle
+            )
+          }
+        }
 
-      BasicTextField(value = viewModel.textInput.value,
-        onValueChange = { viewModel.textInput.value = it },
-        textStyle = shellTextStyle,
-        modifier = Modifier.weight(1f)
-          .onKeyEvent {
-            return@onKeyEvent if (it.nativeKeyEvent.keyCode == KeyEvent.KEYCODE_ENTER) {
-              // TODO prompt
-              viewModel.textInput.value = "youhou"
-              true
-            } else false
-          },
-        singleLine = true,
-        cursorBrush = SolidColor(Color.White),
-      )
-
+        if (prompt.output != null) {
+          item {
+            Text(
+              text = prompt.output,
+              style = shellTextStyle
+            )
+          }
+        }
+      }
     }
+    OutlinedTextField(value = viewModel.textInput.value,
+      onValueChange = { viewModel.textInput.value = it },
+      textStyle = shellTextStyle,
+      keyboardActions = KeyboardActions(onDone = {
+        viewModel.textInput.value = "youhou"
+      }),
+      modifier = Modifier.fillMaxWidth(),
+      shape = RoundedCornerShape(36.dp)
+    )
   }
-
 }
---
+++ marcel-android/app/src/main/java/com/tambapps/marcel/android/marshell/ui/model/Prompt.kt
@@ -0,0 +1,3 @@
+package com.tambapps.marcel.android.marshell.ui.model
+
+data class Prompt(val input: String?, val output: String?)
