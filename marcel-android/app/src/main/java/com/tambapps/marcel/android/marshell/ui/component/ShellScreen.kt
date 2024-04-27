d11ef765967732ecaa7a32c6567f694e726d95f9
Nelson Fonkoua <tambapps@gmail.com>
2024-04-27 05:36:53 +0200

    fixed text editing and started handle history

--- marcel-android/app/src/main/java/com/tambapps/marcel/android/marshell/ui/component/ShellScreen.kt
+++ marcel-android/app/src/main/java/com/tambapps/marcel/android/marshell/ui/component/ShellScreen.kt
@@ -42,6 +42,9 @@ import com.tambapps.marcel.android.marshell.repl.console.PromptPrinter
 import com.tambapps.marcel.android.marshell.ui.theme.TopBarIconSize
 import com.tambapps.marcel.android.marshell.ui.theme.shellTextStyle
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import marcel.lang.Script
 import marcel.lang.util.MarcelVersion
@@ -51,7 +54,7 @@ val HEADER = "Marshell (Marcel: ${MarcelVersion.VERSION}, Android ${Build.VERSIO
 @Composable
 fun ShellScreen(shellSessionFactory: ShellSessionFactory, scope: CoroutineScope = rememberCoroutineScope(), viewModel: ShellViewModel = viewModel(factory = ShellViewModelFactory(shellSessionFactory))) {
   Column(modifier = Modifier.fillMaxSize()) {
-    TopBar()
+    TopBar(viewModel)
     val listState = rememberLazyListState()
     LazyColumn(modifier = Modifier
       .weight(1f)
@@ -86,7 +89,10 @@ fun ShellScreen(shellSessionFactory: ShellSessionFactory, scope: CoroutineScope 
 
     Row(verticalAlignment = Alignment.CenterVertically) {
       OutlinedTextField(value = viewModel.textInput.value,
-        onValueChange = { viewModel.textInput.value = it.copy(annotatedString = viewModel.highlighter.highlight(it.annotatedString).toAnnotatedString()) },
+        onValueChange = {
+          viewModel.textInput.value = it
+          viewModel.highlightTextInput()
+        },
         textStyle = shellTextStyle,
         modifier = Modifier.weight(1f),
         shape = RoundedCornerShape(36.dp)
@@ -97,25 +103,19 @@ fun ShellScreen(shellSessionFactory: ShellSessionFactory, scope: CoroutineScope 
 }
 
 @Composable
-fun TopBar() {
+fun TopBar(viewModel: ShellViewModel) {
   TopBarLayout(horizontalArrangement = Arrangement.End) {
     IconButton(
       imageVector = Icons.Filled.KeyboardArrowUp,
       modifier = Modifier.padding(end = 8.dp),
       size = TopBarIconSize,
-      onClick = {
-
-      }
+      onClick = { viewModel.historyUp() }
     )
     IconButton(
       imageVector = Icons.Filled.KeyboardArrowDown,
       size = TopBarIconSize,
-      onClick = {
-
-      }
+      onClick = { viewModel.historyDown() }
     )
-
-
   }
 }
 @Composable
@@ -155,13 +155,32 @@ class ShellViewModel constructor(private val shellSession: ShellSession) : ViewM
   val textInput = mutableStateOf(TextFieldValue())
   val prompts = mutableStateListOf<Prompt>()
   val isEvaluating = mutableStateOf(false)
-  val highlighter = shellSession.newHighlighter()
+  private val highlighter = shellSession.newHighlighter()
+  private var historyIndex = -1
+  private val highlightScope = CoroutineScope(Dispatchers.IO)
+  private var job: Job? = null
 
   init {
     shellSession.scriptConfigurer = { script: Script ->
       (script as MarshellScript).setPrinter(PromptPrinter(prompts))
     }
   }
+
+  // TODO fix history functions
+  fun historyUp() {
+    val prompts = this.prompts.filter { it.type == Prompt.Type.INPUT }
+    if (prompts.isEmpty() || prompts.size <= historyIndex) return
+    val text = prompts[prompts.size - 1 - ++historyIndex].text
+    textInput.value = textInput.value.copy(annotatedString = highlighter.highlight(text).toAnnotatedString())
+  }
+
+  fun historyDown() {
+    val prompts = this.prompts.filter { it.type == Prompt.Type.INPUT }
+    if (prompts.isEmpty() || historyIndex <= 0) return
+    val text = prompts[prompts.size - 1 - --historyIndex].text
+    textInput.value = textInput.value.copy(annotatedString = highlighter.highlight(text).toAnnotatedString())
+  }
+
   fun prompt(text: String) {
     prompts.add(Prompt(Prompt.Type.INPUT, text))
     textInput.value = TextFieldValue()
@@ -169,6 +188,15 @@ class ShellViewModel constructor(private val shellSession: ShellSession) : ViewM
     shellSession.eval(text) { type, result ->
       isEvaluating.value = false
       prompts.add(Prompt(type, java.lang.String.valueOf(result)))
+    }
+    historyIndex = -1
+  }
+
+  fun highlightTextInput() {
+    job?.cancel()
+    job = highlightScope.launch {
+      delay(500)
+      textInput.value = textInput.value.copy(annotatedString = highlighter.highlight(textInput.value.annotatedString).toAnnotatedString())
     }
   }
 }
