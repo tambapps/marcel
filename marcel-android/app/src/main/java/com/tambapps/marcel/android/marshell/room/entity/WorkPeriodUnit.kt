451f74a720c33da394741b23321b36e92af6f444
Nelson Fonkoua <tambapps@gmail.com>
2024-05-07 00:12:35 +0200

    started implementing period picker

--- marcel-android/app/src/main/java/com/tambapps/marcel/android/marshell/room/entity/WorkPeriodUnit.kt
+++ marcel-android/app/src/main/java/com/tambapps/marcel/android/marshell/room/entity/WorkPeriodUnit.kt
@@ -1,19 +1,13 @@
 package com.tambapps.marcel.android.marshell.room.entity
 
 enum class WorkPeriodUnit {
-  MINUTES, HOURS, DAYS, WEEKS, THIRTY_DAYS {
-    override fun toString(): String {
-      return "30 days"
-    }
-  };
+  MINUTES, HOURS, DAYS;
 
   fun toMinutes(n: Int): Long {
     return n * when (this) {
       MINUTES -> 1L
       HOURS -> 60L
       DAYS -> HOURS.toMinutes(24)
-      WEEKS -> DAYS.toMinutes(7)
-      THIRTY_DAYS -> DAYS.toMinutes(30)
     }
   }
 
--- marcel-android/app/src/main/java/com/tambapps/marcel/android/marshell/ui/screen/work/create/WorkCreateScreen.kt
+++ marcel-android/app/src/main/java/com/tambapps/marcel/android/marshell/ui/screen/work/create/WorkCreateScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Edit
@@ -16,7 +18,10 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CalendarLocale
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerState
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -25,11 +30,13 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.SelectableDates
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TimePicker
 import androidx.compose.material3.TimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -37,11 +44,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.tambapps.marcel.android.marshell.Routes
+import com.tambapps.marcel.android.marshell.room.entity.WorkPeriodUnit
 import com.tambapps.marcel.android.marshell.ui.screen.work.WorkScriptCard
 import com.tambapps.marcel.android.marshell.ui.theme.TopBarHeight
 import com.tambapps.marcel.android.marshell.ui.theme.shellTextStyle
@@ -120,7 +129,14 @@ private fun Form(viewModel: WorkCreateViewModel) {
 
   WorkScriptCard(viewModel = viewModel)
   Box(modifier = Modifier.padding(8.dp))
-  TextIconButton(fieldName = "Period", value = "One-shot", onClick = { /*TODO*/ })
+
+  val showPeriodDialog = remember { mutableStateOf(false) }
+  TextIconButton(fieldName = "Period", value = "One-shot", onClick = { showPeriodDialog.value = true })
+  PeriodPickerDialog(
+    viewModel = viewModel,
+    show = showPeriodDialog,
+    onDismissRequest = { showPeriodDialog.value = false }
+  )
 
   val showSchedulePickerDialog = remember { mutableStateOf(false) }
   TextIconButton(
@@ -150,6 +166,85 @@ private class TodayOrAfter: SelectableDates {
     return year >= nowYear
   }
 }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun PeriodPickerDialog(
+  viewModel: WorkCreateViewModel,
+  show: MutableState<Boolean>,
+  onDismissRequest: () -> Unit
+) {
+  if (!show.value) {
+    return
+  }
+
+  var periodAmount by remember { mutableIntStateOf(1) }
+  val periodState = remember { mutableStateOf(WorkPeriodUnit.HOURS) }
+  AlertDialog(
+    text = {
+      Row(modifier = Modifier.fillMaxWidth()) {
+
+        TextField(value = periodAmount.toString(),
+          keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+          onValueChange = { periodAmount = it.toIntOrNull() ?: 1},
+          modifier = Modifier.width(100.dp))
+        ExposedDropdownMenu(
+          values = WorkPeriodUnit.entries.toList(),
+          selectedItem = periodState
+        )
+
+      }
+    },
+    onDismissRequest = onDismissRequest,
+    dismissButton = {
+      TextButton(onClick = onDismissRequest) {
+        Text("Cancel")
+      }
+    },
+    confirmButton = {
+    }
+  )
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun <T> ExposedDropdownMenu(
+  values: List<T>,
+  selectedItem: MutableState<T>
+) {
+  var dropDownExpanded by remember { mutableStateOf(false) }
+
+  ExposedDropdownMenuBox(
+    modifier = Modifier.width(150.dp),
+    expanded = dropDownExpanded,
+    onExpandedChange = {
+      dropDownExpanded = !dropDownExpanded
+    }
+  ) {
+    TextField(
+      value = selectedItem.value.toString(),
+      onValueChange = {},
+      readOnly = true,
+      trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = dropDownExpanded) },
+      modifier = Modifier.menuAnchor()
+    )
+
+    ExposedDropdownMenu(
+      expanded = dropDownExpanded,
+      onDismissRequest = { dropDownExpanded = false }
+    ) {
+
+      for (item in values) {
+        DropdownMenuItem(
+          text = { Text(text = item.toString()) },
+          onClick = {
+            selectedItem.value = item
+            dropDownExpanded = false
+          }
+        )
+      }
+    }
+  }
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun DateTimePickerDialog(
