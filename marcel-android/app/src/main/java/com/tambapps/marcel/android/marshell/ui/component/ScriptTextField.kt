39dc819535cac935d9d99268edacc1ecaa774f9f
Nelson Fonkoua <tambapps@gmail.com>
2024-05-08 13:05:05 +0200

    moved script text field to dedicated file

---
+++ marcel-android/app/src/main/java/com/tambapps/marcel/android/marshell/ui/component/ScriptTextField.kt
@@ -0,0 +1,92 @@
+package com.tambapps.marcel.android.marshell.ui.component
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.input.key.type
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.tambapps.marcel.android.marshell.ui.screen.work.ScriptCardViewModel
+import com.tambapps.marcel.android.marshell.ui.theme.shellTextStyle
+
+@Composable
+fun ScriptField(viewModel: ScriptCardViewModel, readOnly: Boolean, modifier: Modifier) {
+  var linesText by remember { mutableIntStateOf(1) }
+  val style = remember { shellTextStyle.copy(lineHeight = 26.sp) }
+
+  val linesTextScroll = rememberScrollState()
+  val scriptTextScroll = rememberScrollState()
+
+  // TODO answer to https://stackoverflow.com/questions/76655920/how-to-add-line-numbers-to-basictextfield-in-jetpack-compose
+  //   with this code example
+  // synchronize scrolling
+  LaunchedEffect(linesTextScroll.value) {
+    scriptTextScroll.scrollTo(linesTextScroll.value)
+  }
+  LaunchedEffect(scriptTextScroll.value) {
+    linesTextScroll.scrollTo(scriptTextScroll.value)
+  }
+
+  Row(modifier = modifier) {
+    BasicTextField(
+      modifier = Modifier
+        .fillMaxHeight()
+        .width(12.dp * linesText.toString().length)
+        .verticalScroll(linesTextScroll),
+      value = IntRange(1, linesText).joinToString(separator = "\n"),
+      readOnly = true,
+      textStyle = style.copy(textAlign = TextAlign.End),
+      onValueChange = {})
+
+    VerticalDivider(
+      modifier = Modifier.fillMaxHeight().padding(horizontal = 8.dp),
+      color = Color.White
+    )
+    BasicTextField(
+      modifier = Modifier
+        .fillMaxHeight()
+        .weight(1f)
+        // this is a hack to prevent this https://stackoverflow.com/questions/76287857/when-parent-of-textfield-is-clickable-hardware-enter-return-button-triggers-its
+        .onKeyEvent { it.type == KeyEventType.KeyUp && it.key == Key.Enter }
+        .verticalScroll(scriptTextScroll),
+      value = viewModel.scriptTextInput,
+      readOnly = readOnly,
+      textStyle = style,
+      onValueChange = { textFieldValue ->
+        val nbLines = textFieldValue.annotatedString.count { it == '\n' } + 1
+        if (nbLines != linesText) linesText = nbLines
+        viewModel.onScriptTextChange(textFieldValue)
+      },
+      visualTransformation = viewModel,
+    )
+  }
+  if (viewModel.scriptTextError != null) {
+    Text(
+      modifier = Modifier.fillMaxWidth(),
+      textAlign = TextAlign.Center,
+      text = viewModel.scriptTextError!!,
+      color = MaterialTheme.colorScheme.error
+    )
+  }
+}
--- marcel-android/app/src/main/java/com/tambapps/marcel/android/marshell/ui/screen/work/WorkComponents.kt
+++ marcel-android/app/src/main/java/com/tambapps/marcel/android/marshell/ui/screen/work/WorkComponents.kt
@@ -39,6 +39,7 @@ import androidx.work.WorkInfo
 import com.tambapps.marcel.android.marshell.R
 import com.tambapps.marcel.android.marshell.room.entity.ShellWork
 import com.tambapps.marcel.android.marshell.ui.component.ExpandableCard
+import com.tambapps.marcel.android.marshell.ui.component.ScriptField
 import com.tambapps.marcel.android.marshell.ui.theme.shellTextStyle
 import com.tambapps.marcel.android.marshell.util.TimeUtils
 import java.time.Duration
@@ -73,67 +74,6 @@ fun WorkScriptCard(viewModel: ScriptCardViewModel, readOnly: Boolean = false) {
       }
     }) {
     ScriptField(viewModel = viewModel, readOnly = readOnly, modifier = Modifier.weight(1f).padding(top = 8.dp))
-  }
-}
-
-@Composable
-fun ScriptField(viewModel: ScriptCardViewModel, readOnly: Boolean, modifier: Modifier) {
-  var linesText by remember { mutableIntStateOf(1) }
-  val style = remember { shellTextStyle.copy(lineHeight = 26.sp) }
-
-  val linesTextScroll = rememberScrollState()
-  val scriptTextScroll = rememberScrollState()
-
-  // TODO answer to https://stackoverflow.com/questions/76655920/how-to-add-line-numbers-to-basictextfield-in-jetpack-compose
-  //   with this code example
-  // synchronize scrolling
-  LaunchedEffect(linesTextScroll.value) {
-    scriptTextScroll.scrollTo(linesTextScroll.value)
-  }
-  LaunchedEffect(scriptTextScroll.value) {
-    linesTextScroll.scrollTo(scriptTextScroll.value)
-  }
-
-  Row(modifier = modifier) {
-    BasicTextField(
-      modifier = Modifier
-        .fillMaxHeight()
-        .width(12.dp * linesText.toString().length)
-        .verticalScroll(linesTextScroll),
-      value = IntRange(1, linesText).joinToString(separator = "\n"),
-      readOnly = true,
-      textStyle = style.copy(textAlign = TextAlign.End),
-      onValueChange = {})
-
-    VerticalDivider(
-      modifier = Modifier.fillMaxHeight().padding(horizontal = 8.dp),
-      color = Color.White
-    )
-    BasicTextField(
-      modifier = Modifier
-        .fillMaxHeight()
-        .weight(1f)
-        // this is a hack to prevent this https://stackoverflow.com/questions/76287857/when-parent-of-textfield-is-clickable-hardware-enter-return-button-triggers-its
-        .onKeyEvent { it.type == KeyEventType.KeyUp && it.key == Key.Enter }
-        .verticalScroll(scriptTextScroll),
-      value = viewModel.scriptTextInput,
-      readOnly = readOnly,
-      textStyle = style,
-      onValueChange = { textFieldValue ->
-        val nbLines = textFieldValue.annotatedString.count { it == '\n' } + 1
-        if (nbLines != linesText) linesText = nbLines
-        viewModel.onScriptTextChange(textFieldValue)
-      },
-      visualTransformation = viewModel,
-    )
-  }
-  if (viewModel.scriptTextError != null) {
-    Text(
-      modifier = Modifier.fillMaxWidth(),
-      textAlign = TextAlign.Center,
-      text = viewModel.scriptTextError!!,
-      color = MaterialTheme.colorScheme.error
-    )
   }
 }
 
