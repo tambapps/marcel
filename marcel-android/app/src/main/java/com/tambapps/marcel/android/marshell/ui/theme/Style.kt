972882890c39b1121140ca7bc30a6495a38558bd
Nelson Fonkoua <tambapps@gmail.com>
2024-05-11 13:42:49 +0200

    used font from googlefonts

--- marcel-android/app/src/main/java/com/tambapps/marcel/android/marshell/ui/theme/Style.kt
+++ marcel-android/app/src/main/java/com/tambapps/marcel/android/marshell/ui/theme/Style.kt
@@ -8,12 +8,9 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.tambapps.marcel.android.marshell.R
 
-private val ubuntuFont = FontFamily(
-  Font(R.font.ubuntu_font, FontWeight.Normal),
-)
 
 val shellTextStyle = TextStyle(
-  fontFamily = ubuntuFont,
+  fontFamily = displayFontFamily,
   fontWeight = FontWeight.Light,
   fontSize = 18.sp,
   color = Color.White
