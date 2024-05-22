fae1f44ae7defe4f7cdeabd4c2cecc1ec31586e6
Nelson Fonkoua <tambapps@gmail.com>
2024-05-22 22:41:00 +0200

    bumped dependencies

--- marcel-android/app/build.gradle.kts
+++ marcel-android/app/build.gradle.kts
@@ -69,7 +69,7 @@ dependencies {
   implementation(project(path = ":marcel-dalvik-stdlib"))
 
   implementation("androidx.core:core-ktx:1.13.1")
-  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
   implementation("androidx.activity:activity-compose:1.9.0")
   implementation("androidx.navigation:navigation-compose:2.7.7")
   implementation(platform("androidx.compose:compose-bom:2024.05.00"))
@@ -77,7 +77,7 @@ dependencies {
   implementation("androidx.compose.ui:ui-graphics")
   implementation("androidx.compose.ui:ui-tooling-preview")
   implementation("androidx.compose.material3:material3")
-  implementation("androidx.datastore:datastore-preferences:1.1.0") // TODO delete this, just use preferences API
+  implementation("androidx.datastore:datastore-preferences:1.1.1")
   implementation("androidx.work:work-runtime-ktx:2.9.0")
   implementation("androidx.compose.ui:ui-text-google-fonts:1.6.7")
   implementation("androidx.browser:browser:1.8.0")
@@ -99,7 +99,7 @@ dependencies {
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-  androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.01"))
+  androidTestImplementation(platform("androidx.compose:compose-bom:2024.05.00"))
   androidTestImplementation("androidx.compose.ui:ui-test-junit4")
   debugImplementation("androidx.compose.ui:ui-tooling")
   debugImplementation("androidx.compose.ui:ui-test-manifest")
