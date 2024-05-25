e661886944a5e130ecc0c091afdd1d1f9114949e
Nelson Fonkoua <tambapps@gmail.com>
2024-05-25 18:54:33 +0200

    bumped version

--- marcel-android/app/build.gradle.kts
+++ marcel-android/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
     applicationId = "com.tambapps.marcel.android.marshell"
     minSdk = 30
     targetSdk = 34
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 4
+    versionName = "1.0.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables {
--- marcel-stdlib/src/main/java/marcel/lang/util/MarcelVersion.java
+++ marcel-stdlib/src/main/java/marcel/lang/util/MarcelVersion.java
@@ -2,5 +2,5 @@ package marcel.lang.util;
 
 public final class MarcelVersion {
 
-  public static final String VERSION = "0.1.2";
+  public static final String VERSION = "1.0";
 }
