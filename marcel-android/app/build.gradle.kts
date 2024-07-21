410a53d389e75e87a3e233a455ff2494c2916b00
Nelson Fonkoua <tambapps@gmail.com>
2024-07-21 15:37:34 +0200

    bumped android dependencies

--- marcel-android/app/build.gradle.kts
+++ marcel-android/app/build.gradle.kts
@@ -70,17 +70,17 @@ dependencies {
   implementation(project(path = ":marcel-dalvik-stdlib"))
 
   implementation("androidx.core:core-ktx:1.13.1")
-  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
+  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
   implementation("androidx.activity:activity-compose:1.9.0")
   implementation("androidx.navigation:navigation-compose:2.7.7")
-  implementation(platform("androidx.compose:compose-bom:2024.05.00"))
+  implementation(platform("androidx.compose:compose-bom:2024.06.00"))
   implementation("androidx.compose.ui:ui")
   implementation("androidx.compose.ui:ui-graphics")
   implementation("androidx.compose.ui:ui-tooling-preview")
   implementation("androidx.compose.material3:material3")
   implementation("androidx.datastore:datastore-preferences:1.1.1")
   implementation("androidx.work:work-runtime-ktx:2.9.0")
-  implementation("androidx.compose.ui:ui-text-google-fonts:1.6.7")
+  implementation("androidx.compose.ui:ui-text-google-fonts:1.6.8")
   implementation("androidx.browser:browser:1.8.0")
 
   // hilt
@@ -98,9 +98,8 @@ dependencies {
 
   // tests
   testImplementation("junit:junit:4.13.2")
-  androidTestImplementation("androidx.test.ext:junit:1.1.5")
-  androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-  androidTestImplementation(platform("androidx.compose:compose-bom:2024.05.00"))
+  androidTestImplementation("androidx.test.ext:junit:1.2.1")
+  androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
   androidTestImplementation("androidx.compose.ui:ui-test-junit4")
   debugImplementation("androidx.compose.ui:ui-tooling")
   debugImplementation("androidx.compose.ui:ui-test-manifest")
