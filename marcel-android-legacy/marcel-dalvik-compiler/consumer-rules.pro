de5cdcd57b10b0fe4bdae5a1ec3b3d3a8b939fd6
Nelson Fonkoua <tambapps@gmail.com>
2024-07-26 20:07:02 +0200

    removed android legacy project

--- marcel-android-legacy/marcel-dalvik-compiler/consumer-rules.pro
+++
@@ -1,1 +0,0 @@
--keep class com.tambapps.marcel.dalvik.compiler.** { *; }
--- marcel-android-legacy/marcel-dalvik-compiler/src/main/java/com/tambapps/marcel/dalvik/compiler/DexException.java
+++
@@ -1,7 +0,0 @@
-package com.tambapps.marcel.dalvik.compiler;
-
-public class DexException extends RuntimeException {
-  public DexException(String message) {
-    super(message);
-  }
-}
