cf708dfefb81f2cb644ae5d59cfe8a6b54ad11a5
Nelson Fonkoua <tambapps@gmail.com>
2024-12-06 19:10:36 +0100

    allowed to define own equals and/or hashCode method even with data annotation

--- marcel-compilation/marcel-semantic/marcel-semantic-transformer/src/main/kotlin/com/tambapps/marcel/semantic/transform/EqualsAndHashcodeAstTransformation.kt
+++ marcel-compilation/marcel-semantic/marcel-semantic-transformer/src/main/kotlin/com/tambapps/marcel/semantic/transform/EqualsAndHashcodeAstTransformation.kt
@@ -1,5 +1,6 @@
 package com.tambapps.marcel.semantic.transform
 
+import com.tambapps.marcel.parser.cst.ClassCstNode
 import com.tambapps.marcel.parser.cst.CstNode
 import com.tambapps.marcel.semantic.ast.Annotable
 import com.tambapps.marcel.semantic.ast.AnnotationNode
@@ -26,13 +27,27 @@ class EqualsAndHashcodeAstTransformation : GenerateMethodAstTransformation() {
     javaType: SourceJavaType,
     annotation: AnnotationNode
   ): List<MarcelMethod> {
-    return listOf(
-      signature(name = "equals", parameters = listOf(parameter(JavaType.Object, "obj")), returnType = JavaType.boolean),
-      signature(name = "hashCode", returnType = JavaType.int)
-    )
+    val signatures = mutableListOf<MarcelMethod>()
+    if (node !is ClassCstNode
+      || node.methods.none { method -> method.name == "equals" && method.parameters.size == 1
+          && method.parameters.first().type.let { it.value == "Object" || it.value == "java.lan.Object" }
+          && method.returnTypeNode.value == "boolean"
+      }
+    ) {
+      signatures.add(signature(name = "equals", parameters = listOf(parameter(JavaType.Object, "obj")), returnType = JavaType.boolean))
+    }
+    if (node !is ClassCstNode
+      || node.methods.none { method -> method.name == "hashCode" && method.parameters.isEmpty()
+          && method.returnTypeNode.value == "int"
+      }
+    ) {
+      signatures.add(signature(name = "hashCode", returnType = JavaType.int))
+    }
+    return signatures
   }
 
   override fun generateMethodNodes(node: AstNode, classNode: ClassNode, annotation: AnnotationNode): List<MethodNode> {
+    val generatedMethods = mutableListOf<MethodNode>()
     val fields = classNode.fields.filter { !isAnnotableExcluded(it) && !it.isStatic }
     val methods =
       // only handle method properties if field is enabled
@@ -40,72 +55,79 @@ class EqualsAndHashcodeAstTransformation : GenerateMethodAstTransformation() {
         classNode.methods.filter { !isAnnotableExcluded(it) && !it.isStatic && it.isGetter }
       else emptyList()
 
-    val equalsMethod = methodNode(
-      ownerClass = classNode.type, name = "equals",
-      parameters = listOf(parameter(JavaType.Object, "obj")),
-      returnType = JavaType.boolean,
-      annotations = listOf(annotationNode(Override::class.javaAnnotationType))
-    ) {
-      val argRef = lvRef("obj")
-      ifStmt(isEqualExpr(argRef, thisRef())) {
-        returnStmt(bool(true))
-      }
-      if (classNode.superType != JavaType.Object) {
-        ifStmt(notExpr(fCall(name = "equals", owner = superRef(), arguments = listOf(argRef)))) {
+    if (classNode.methods.none { it.name == "equals" && it.parameters.firstOrNull()?.type == JavaType.Object && it.returnType == JavaType.boolean }) {
+      val equalsMethod = methodNode(
+        ownerClass = classNode.type, name = "equals",
+        parameters = listOf(parameter(JavaType.Object, "obj")),
+        returnType = JavaType.boolean,
+        annotations = listOf(annotationNode(Override::class.javaAnnotationType))
+      ) {
+        val argRef = lvRef("obj")
+        ifStmt(isEqualExpr(argRef, thisRef())) {
+          returnStmt(bool(true))
+        }
+        if (classNode.superType != JavaType.Object) {
+          ifStmt(notExpr(fCall(name = "equals", owner = superRef(), arguments = listOf(argRef)))) {
+            returnStmt(bool(false))
+          }
+        }
+        ifStmt(notExpr(isInstanceExpr(classNode.type, argRef))) {
           returnStmt(bool(false))
         }
-      }
-      ifStmt(notExpr(isInstanceExpr(classNode.type, argRef))) {
-        returnStmt(bool(false))
-      }
-      // now we know it is the right type
-      val otherVar = currentMethodScope.addLocalVariable(classNode.type)
-      varAssignStmt(otherVar, argRef)
+        // now we know it is the right type
+        val otherVar = currentMethodScope.addLocalVariable(classNode.type)
+        varAssignStmt(otherVar, argRef)
 
-      val otherRef = ref(otherVar)
-      for (field in fields) {
-        ifStmt(notEqual(ref(field), ref(field, owner = otherRef))) {
-          returnStmt(bool(false))
-        }
-      }
-      for (method in methods) {
-        val ownProperty = fCall(method = method, owner = thisRef(), arguments = emptyList())
-        val otherProperty = fCall(method = method, owner = otherRef, arguments = emptyList())
-        ifStmt(notEqual(ownProperty, otherProperty)) {
-          returnStmt(bool(false))
-        }
-      }
-      returnStmt(bool(true))
-    }
-    val hashCode = methodNode(
-      ownerClass = classNode.type,
-      name = "hashCode",
-      returnType = JavaType.int,
-      annotations = listOf(annotationNode(Override::class.javaAnnotationType))
-    ) {
-      if (fields.isEmpty() && methods.isEmpty()) {
-        returnStmt(fCall(name = "hashCode", owner = superRef(), arguments = emptyList()))
-      } else {
-        val resultVar = currentMethodScope.addLocalVariable(JavaType.int)
-        varAssignStmt(
-          resultVar,
-          if (classNode.superType == JavaType.Object) int(0) else fCall(
-            owner = superRef(),
-            name = "hashCode",
-            arguments = emptyList()
-          )
-        )
+        val otherRef = ref(otherVar)
         for (field in fields) {
-          varAssignStmt(resultVar, plus(mul(int(31), ref(resultVar)), hash(ref(field))))
+          ifStmt(notEqual(ref(field), ref(field, owner = otherRef))) {
+            returnStmt(bool(false))
+          }
         }
         for (method in methods) {
-          val methodProperty = fCall(method = method, owner = thisRef(), arguments = emptyList())
-          varAssignStmt(resultVar, plus(mul(int(31), ref(resultVar)), hash(methodProperty)))
+          val ownProperty = fCall(method = method, owner = thisRef(), arguments = emptyList())
+          val otherProperty = fCall(method = method, owner = otherRef, arguments = emptyList())
+          ifStmt(notEqual(ownProperty, otherProperty)) {
+            returnStmt(bool(false))
+          }
         }
-        returnStmt(ref(resultVar))
+        returnStmt(bool(true))
       }
+      generatedMethods.add(equalsMethod)
     }
-    return listOf(equalsMethod, hashCode)
+
+    if (classNode.methods.none { it.name == "hashCode" && it.parameters.isEmpty() && it.returnType == JavaType.int }) {
+      val hashCode = methodNode(
+        ownerClass = classNode.type,
+        name = "hashCode",
+        returnType = JavaType.int,
+        annotations = listOf(annotationNode(Override::class.javaAnnotationType))
+      ) {
+        if (fields.isEmpty() && methods.isEmpty()) {
+          returnStmt(fCall(name = "hashCode", owner = superRef(), arguments = emptyList()))
+        } else {
+          val resultVar = currentMethodScope.addLocalVariable(JavaType.int)
+          varAssignStmt(
+            resultVar,
+            if (classNode.superType == JavaType.Object) int(0) else fCall(
+              owner = superRef(),
+              name = "hashCode",
+              arguments = emptyList()
+            )
+          )
+          for (field in fields) {
+            varAssignStmt(resultVar, plus(mul(int(31), ref(resultVar)), hash(ref(field))))
+          }
+          for (method in methods) {
+            val methodProperty = fCall(method = method, owner = thisRef(), arguments = emptyList())
+            varAssignStmt(resultVar, plus(mul(int(31), ref(resultVar)), hash(methodProperty)))
+          }
+          returnStmt(ref(resultVar))
+        }
+      }
+      generatedMethods.add(hashCode)
+    }
+    return generatedMethods
   }
 
   private fun hash(propertyExpr: ExpressionNode): ExpressionNode {
