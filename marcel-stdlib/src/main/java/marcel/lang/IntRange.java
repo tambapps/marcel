18bc813b2396ac350ebdc955fe7ac999cf2bdfd7
Nelson Fonkoua <tambapps@gmail.com>
2025-04-30 23:27:53 +0200

    added methods to ranges

--- marcel-stdlib/src/main/java/marcel/lang/IntRange.java
+++ marcel-stdlib/src/main/java/marcel/lang/IntRange.java
@@ -1,10 +1,26 @@
 package marcel.lang;
 
-
+import marcel.util.function.ToCharFunction;
+import marcel.util.function.ToFloatFunction;
+import marcel.util.primitives.collections.lists.CharArrayList;
+import marcel.util.primitives.collections.lists.CharList;
+import marcel.util.primitives.collections.lists.DoubleArrayList;
+import marcel.util.primitives.collections.lists.DoubleList;
+import marcel.util.primitives.collections.lists.FloatArrayList;
+import marcel.util.primitives.collections.lists.FloatList;
 import marcel.util.primitives.collections.lists.IntArrayList;
 import marcel.util.primitives.collections.lists.IntList;
+import marcel.util.primitives.collections.lists.LongArrayList;
+import marcel.util.primitives.collections.lists.LongList;
 import marcel.util.primitives.iterable.IntIterable;
 import marcel.util.primitives.iterators.IntIterator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.IntFunction;
+import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
 
 public interface IntRange extends IntIterable {
 
@@ -29,6 +45,59 @@ public interface IntRange extends IntIterable {
     return list;
   }
 
+  default <T> List<T> map(IntFunction<T> mapper) {
+    List<T> list = new ArrayList<>();
+    IntIterator iterator = iterator();
+    while (iterator.hasNext()) {
+      list.add(mapper.apply(iterator.nextInt()));
+    }
+    return list;
+  }
+
+  default IntList mapToInt(IntFunction<Integer> mapper) {
+    IntList list = new IntArrayList();
+    IntIterator iterator = iterator();
+    while (iterator.hasNext()) {
+      list.add(mapper.apply(iterator.nextInt()));
+    }
+    return list;
+  }
+
+  default LongList mapToLong(IntFunction<Long> mapper) {
+    LongList list = new LongArrayList();
+    IntIterator iterator = iterator();
+    while (iterator.hasNext()) {
+      list.add(mapper.apply(iterator.nextInt()));
+    }
+    return list;
+  }
+
+  default FloatList mapToFloat(IntFunction<Float> mapper) {
+    FloatList list = new FloatArrayList();
+    IntIterator iterator = iterator();
+    while (iterator.hasNext()) {
+      list.add(mapper.apply(iterator.nextInt()));
+    }
+    return list;
+  }
+
+  default DoubleList mapToDouble(IntFunction<Double> mapper) {
+    DoubleList list = new DoubleArrayList();
+    IntIterator iterator = iterator();
+    while (iterator.hasNext()) {
+      list.add(mapper.apply(iterator.nextInt()));
+    }
+    return list;
+  }
+
+  default CharList mapToChar(IntFunction<Character> mapper) {
+    CharList list = new CharArrayList();
+    IntIterator iterator = iterator();
+    while (iterator.hasNext()) {
+      list.add(mapper.apply(iterator.nextInt()));
+    }
+    return list;
+  }
 
   default boolean contains(int i) {
     return isReverse() ? i >= getTo() && i <= getFrom() : i >= getFrom() && i <= getTo();
--- marcel-stdlib/src/main/java/marcel/lang/LongRange.java
+++ marcel-stdlib/src/main/java/marcel/lang/LongRange.java
@@ -1,9 +1,21 @@
 package marcel.lang;
 
+import marcel.util.primitives.collections.lists.CharArrayList;
+import marcel.util.primitives.collections.lists.CharList;
+import marcel.util.primitives.collections.lists.DoubleArrayList;
+import marcel.util.primitives.collections.lists.DoubleList;
+import marcel.util.primitives.collections.lists.FloatArrayList;
+import marcel.util.primitives.collections.lists.FloatList;
+import marcel.util.primitives.collections.lists.IntArrayList;
+import marcel.util.primitives.collections.lists.IntList;
 import marcel.util.primitives.collections.lists.LongArrayList;
 import marcel.util.primitives.collections.lists.LongList;
 import marcel.util.primitives.iterable.LongIterable;
 import marcel.util.primitives.iterators.LongIterator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.LongFunction;
 
 public interface LongRange extends LongIterable {
 
@@ -28,6 +40,59 @@ public interface LongRange extends LongIterable {
     return list;
   }
 
+  default <T> List<T> map(LongFunction<T> mapper) {
+    List<T> list = new ArrayList<>();
+    LongIterator iterator = iterator();
+    while (iterator.hasNext()) {
+      list.add(mapper.apply(iterator.nextLong()));
+    }
+    return list;
+  }
+
+  default IntList mapToInt(LongFunction<Integer> mapper) {
+    IntList list = new IntArrayList();
+    LongIterator iterator = iterator();
+    while (iterator.hasNext()) {
+      list.add(mapper.apply(iterator.nextLong()));
+    }
+    return list;
+  }
+
+  default LongList mapToLong(LongFunction<Long> mapper) {
+    LongList list = new LongArrayList();
+    LongIterator iterator = iterator();
+    while (iterator.hasNext()) {
+      list.add(mapper.apply(iterator.nextLong()));
+    }
+    return list;
+  }
+
+  default FloatList mapToFloat(LongFunction<Float> mapper) {
+    FloatList list = new FloatArrayList();
+    LongIterator iterator = iterator();
+    while (iterator.hasNext()) {
+      list.add(mapper.apply(iterator.nextLong()));
+    }
+    return list;
+  }
+
+  default DoubleList mapToDouble(LongFunction<Double> mapper) {
+    DoubleList list = new DoubleArrayList();
+    LongIterator iterator = iterator();
+    while (iterator.hasNext()) {
+      list.add(mapper.apply(iterator.nextLong()));
+    }
+    return list;
+  }
+
+  default CharList mapToChar(LongFunction<Character> mapper) {
+    CharList list = new CharArrayList();
+    LongIterator iterator = iterator();
+    while (iterator.hasNext()) {
+      list.add(mapper.apply(iterator.nextLong()));
+    }
+    return list;
+  }
 
 
   default boolean contains(long i) {
