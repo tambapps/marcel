5bb1eec58e3eb89b04de88ea964bc3fbb4b3fee6
Nelson Fonkoua <tambapps@gmail.com>
2025-06-21 12:29:33 +0200

    bump for release

--- marcel-android/app/build.gradle.kts
+++ marcel-android/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     applicationId = "com.tambapps.marcel.android.marshell"
     minSdk = 30
     targetSdk = 35
-    versionCode = 13
+    versionCode = 14
     versionName = "1.1.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -81,14 +81,14 @@ dependencies {
   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
   implementation("androidx.activity:activity-compose:1.10.1")
   implementation("androidx.navigation:navigation-compose:2.9.0")
-  implementation(platform("androidx.compose:compose-bom:2025.06.00"))
+  implementation(platform("androidx.compose:compose-bom:2025.06.01"))
   implementation("androidx.compose.ui:ui")
   implementation("androidx.compose.ui:ui-graphics")
   implementation("androidx.compose.ui:ui-tooling-preview")
   implementation("androidx.compose.material3:material3")
   implementation("androidx.datastore:datastore-preferences:1.1.7")
-  implementation("androidx.work:work-runtime-ktx:2.10.1")
-  implementation("androidx.compose.ui:ui-text-google-fonts:1.8.2")
+  implementation("androidx.work:work-runtime-ktx:2.10.2")
+  implementation("androidx.compose.ui:ui-text-google-fonts:1.8.3")
   implementation("androidx.browser:browser:1.8.0")
 
   implementation("org.commonmark:commonmark:0.22.0")
