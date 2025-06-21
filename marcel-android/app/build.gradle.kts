c52d2fb64f75feff0ce27ce44580b8c3f76af9ad
Nelson Fonkoua <tambapps@gmail.com>
2025-06-21 12:35:28 +0200

    bumped for development

--- marcel-android/app/build.gradle.kts
+++ marcel-android/app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
     minSdk = 30
     targetSdk = 35
     versionCode = 14
-    versionName = "1.1.0"
+    versionName = "1.2.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables {
--- marcel-stdlib/src/main/java/marcel/util/MarcelVersion.java
+++ marcel-stdlib/src/main/java/marcel/util/MarcelVersion.java
@@ -2,5 +2,5 @@ package marcel.util;
 
 public final class MarcelVersion {
 
-  public static final String VERSION = "1.1";
+  public static final String VERSION = "1.2";
 }
